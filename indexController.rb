2fb51f8c18fce13de613495ddaecc7bf6fcf7cba
fliiix <de-ch@hotmail.de>
2013-04-20 23:55:38 +0200

    start with text posts and nice urls

--- indexController.rb
+++ indexController.rb
@@ -2,81 +2,111 @@
 require "sinatra"
 require "soundcloud"
 require "yaml"
+require "maruku"
 require_relative "model.rb"
 
 configure :development do
-	AppConfig = YAML.load_file(File.expand_path("config.yaml", File.dirname(__FILE__)))["development"]
-	MongoMapper.database = 'music'
-	set :show_exceptions, true
+  AppConfig = YAML.load_file(File.expand_path("config.yaml", File.dirname(__FILE__)))["development"]
+  MongoMapper.database = 'music'
+  set :show_exceptions, true
 end
 
 configure :production do
-	AppConfig = YAML.load_file(File.expand_path("config.yaml", File.dirname(__FILE__)))["production"]
+  AppConfig = YAML.load_file(File.expand_path("config.yaml", File.dirname(__FILE__)))["production"]
+end
+
+get "/:id" do |id|
+  @text = BlogPost.find(id)
+  @music = MusicPost.find(id)
+  erb :index
 end
 
 get "/" do
-	@posts = MusicPost.all()
-	erb :index
-end
-
-get "/auth" do
-	# create client object with app credentials
-	client = Soundcloud.new(:client_id => AppConfig["SoundCloudClientId"],
-							:client_secret => AppConfig["SoundCloudClientSecret"],
-							:redirect_uri => 'http://localhost:9393/authPoint')
-
-	# redirect user to authorize URL
-	puts client.authorize_url()
-	redirect client.authorize_url()
-end
-
-get "/authPoint" do
-	# create client object with app credentials
-	client = Soundcloud.new(:client_id => AppConfig["SoundCloudClientId"],
-							:client_secret => AppConfig["SoundCloudClientSecret"],
-							:redirect_uri => 'http://localhost:9393/authPoint')
-	# exchange authorization code for access token
-	auth = client.exchange_token(:code => params[:code])
-	
-	if !client#expired?
-		x = "true"
-	else
-		x = "false"
-	end
-
-	puts "expired: " + x
-	puts "user: " + client.get('/me').username
-
-	puts "access_token hash: " + auth[:access_token]
-	puts "expires_in hash: " + auth[:expires_in].to_s
-	puts "refresh_token hash: " + auth[:refresh_token]
-
-	token = SoundCloudToken.new(:access_token => auth[:access_token], 
-								:refresh_token => auth[:refresh_token])
-	token.save
-	redirect "/add/music"
+  @posts = MusicPost.all
+  @blog = BlogPost.all
+  erb :index
 end
 
 get "/add/:type" do |type|
-	if type == "music"
-		erb :addMusic
-	end
+  if type == "music"
+    erb :addMusic
+  end
+  if type == "text"
+    erb :addText
+  end
+end
+
+post "/add/text" do
+  post = BlogPost.new(:title => params[:title], 
+                      :text => params[:mdtext], 
+                      :urls => NiceUrl.new(:niceUrl => params[:title]))
+  if post.valid?
+    puts "Yeah its valid"
+  else
+    puts post.errors.first
+  end
+
+  if post.save
+    @meldung = "successfully saved"
+  else
+    @meldung = "Error(s): " + post.errors.map {|k,v| "#{k}: #{v}"}.to_s
+  end
+  erb :addText
 end
 
 post "/add/music" do
-	if params[:soundFile] != nil && params[:soundSample] != nil
-		post = MusicPost.new(:title => params[:title], 
-						 :description => params[:description], 
-						 :preis => params[:preis], 
-						 :downloadFileName => params[:soundFile][:tempfile].to_path + "," + params[:soundFile][:filename].to_s,
-						 :soundCloudUrl => params[:soundSample][:tempfile].to_path)
-		if post.save
-			@meldung = "successfully saved"
-		else
-			@meldung = "Error(s): " + post.errors.map {|k,v| "#{k}: #{v}"}.to_s
-		end
-	else
-		@meldung = "Error es muss ein Sample file und ein File ausgewählt werden!"
-	end
-	erb :addMusic
+  if params[:soundFile] != nil && params[:soundSample] != nil
+    post = MusicPost.new(:title => params[:title], 
+             :description => params[:description], 
+             :preis => params[:preis], 
+             :downloadFileName => params[:soundFile][:tempfile].to_path + "," + params[:soundFile][:filename].to_s,
+             :soundCloudUrl => params[:soundSample][:tempfile].to_path,
+             :urls => NiceUrl.new(:url => params[:title]))
+    if post.save
+      @meldung = "successfully saved"
+    else
+      @meldung = "Error(s): " + post.errors.map {|k,v| "#{k}: #{v}"}.to_s
+    end
+  else
+    @meldung = "Error es muss ein Sample file und ein File ausgewählt werden!"
+  end
+  erb :addMusic
+end
+
+get "/auth" do
+  # create client object with app credentials
+  client = Soundcloud.new(:client_id => AppConfig["SoundCloudClientId"],
+                          :client_secret => AppConfig["SoundCloudClientSecret"],
+                          :redirect_uri => 'http://localhost:9393/authPoint')
+
+  # redirect user to authorize URL
+  puts client.authorize_url()
+  redirect client.authorize_url()
+end
+
+get "/authPoint" do
+  # create client object with app credentials
+  client = Soundcloud.new(:client_id => AppConfig["SoundCloudClientId"],
+                          :client_secret => AppConfig["SoundCloudClientSecret"],
+                          :redirect_uri => 'http://localhost:9393/authPoint')
+  # exchange authorization code for access token
+  auth = client.exchange_token(:code => params[:code])
+  
+  if !client#expired?
+    x = "true"
+  else
+    x = "false"
+  end
+
+  puts "expired: " + x
+  puts "user: " + client.get('/me').username
+
+  puts "access_token hash: " + auth[:access_token]
+  puts "expires_in hash: " + auth[:expires_in].to_s
+  puts "refresh_token hash: " + auth[:refresh_token]
+
+  token = SoundCloudToken.new(:access_token => auth[:access_token], 
+                              :refresh_token => auth[:refresh_token])
+  token.save
+  redirect "/add/music"
 end
--- model.rb
+++ model.rb
@@ -1,63 +1,107 @@
+# encoding: UTF-8
 require "mongo_mapper"
 require "fileutils"
 require "soundcloud"
 
 class SoundCloudToken
-	include MongoMapper::Document
+  include MongoMapper::Document
 
-	key :access_token,  String, :require => true
-	key :refresh_token, String, :require => true
+  key :access_token,  String, :require => true
+  key :refresh_token, String, :require => true
+end
+
+class BlogPost
+  include MongoMapper::Document
+
+  key :titel, String, :require => true, :minimum => 4, :maximum => 40
+  key :text,  String, :require => true
+  many :niceUrl, :as => :niceUrls
 end
 
 class MusicPost
-	include MongoMapper::Document
+  include MongoMapper::Document
 
-	key :title,				String, :require => true, :length => 5..25
-	key :description, 		String, :length => 0..800
-	key :preis, 			Float, :require => true, :numeric => true
-	key :downloadFileName, 	String, :require => true
-	key :soundCloudUrl, 	String, :require => true
-	key :SoundCloudId, 		Float, :require => true, :numeric => true
-	timestamps!
+  key :title,             String, :require => true, :minimum => 4, :maximum => 40
+  key :description,       String, :minimum => 4, :maximum => 10000
+  key :preis,             Float,  :require => true, :numeric => true
+  key :downloadFileName,  String, :require => true
+  key :soundCloudUrl,     String, :require => true
+  key :SoundCloudId,      Float,  :require => true, :numeric => true
+  many :niceUrl, :as => :niceUrls
 
-	before_validation :uploadFile
-	before_validation :uploadToSoundCloud
+  before_validation :uploadFile
+  before_validation :uploadToSoundCloud
 
-	private
-	def uploadToSoundCloud
-		lastToken = SoundCloudToken.last()
-		if lastToken != nil
-			client = Soundcloud.new(:access_token => lastToken.access_token)
-			if !client#expired?
-				#need to refresh the token!
-				client = Soundcloud.new(:client_id => AppConfig["SoundCloudClientId"],
-										:client_secret => AppConfig["SoundCloudClientSecret"],
-										:refresh_token => lastToken.refresh_token)
+  private
+  def uploadToSoundCloud
+    lastToken = SoundCloudToken.last()
+    if lastToken != nil
+      client = Soundcloud.new(:access_token => lastToken.access_token)
+      if !client#expired?
+        #need to refresh the token!
+        client = Soundcloud.new(:client_id => AppConfig["SoundCloudClientId"],
+                    :client_secret => AppConfig["SoundCloudClientSecret"],
+                    :refresh_token => lastToken.refresh_token)
 
-				newToken = SoundCloudToken.new(:access_token => client.access_token, 
-											   :refresh_token => client.refresh_token)
-				newToken.save
-			end
-			puts soundCloudUrl
-			track = client.post('/tracks', :track => {
-			  :title => title,
-			  :asset_data => File.new(soundCloudUrl, 'rb')
-			})
-			set[:soundCloudUrl] = track.permalink_url
-			set[:SoundCloudId] = track.id
-		else
-			errors.add(:soundCloudUrl, "No Soundcloud token!")
-		end
-	end
+        newToken = SoundCloudToken.new(:access_token => client.access_token, 
+                         :refresh_token => client.refresh_token)
+        newToken.save
+      end
+      puts soundCloudUrl
+      # puts "file ist " + File.exist?(soundCloudUrl)
+      track = client.post('/tracks', :track => {
+        :title => "TestFile",
+        :asset_data => File.new(soundCloudUrl, 'rb')
+      })
+      self[:soundCloudUrl] = track.permalink_url
+      self[:SoundCloudId] = track.id
+    else
+      errors.add(:soundCloudUrl, "No Soundcloud token!")
+    end
+  end
 
-	def uploadFile
-		fileArray = downloadFileName.split(",")
-		begin
-			filename = rand(36**8).to_s(36) + fileArray[1]
-			FileUtils.cp(fileArray[0], File.expand_path(filename, File.dirname(__FILE__) + "/public/uploads/"))
-			self[:downloadFileName] = filename
-		rescue Exception => e
-			errors.add(:downloadFileName, "Ex: " + e.to_s + " Alles: " + downloadFileName.to_s)
-		end
-	end
+  def uploadFile
+    fileArray = downloadFileName.split(",")
+    begin
+      filename = rand(36**8).to_s(36) + fileArray[1]
+      FileUtils.cp(fileArray[0], File.expand_path(filename, File.dirname(__FILE__) + "/public/uploads/"))
+      self[:downloadFileName] = filename
+    rescue Exception => e
+      errors.add(:downloadFileName, "Ex: " + e.to_s + " Alles: " + downloadFileName.to_s)
+    end
+  end
+end
+
+class NiceUrl
+  include MongoMapper::Document
+
+  key :niceUrl, String, :require => true
+
+  belongs_to :niceUrls, :polymorphic => true
+  before_validation :makeUrlNice
+
+  private
+  def makeUrlNice
+    if niceUrl == nil
+      return nil
+    end
+    
+    puts sanitize("hdsfsdf hdfsdhfkhsdfsd  f hdsfjsdf") + "..."
+    url = sanitize(niceUrl)
+
+
+    puts sanitize(niceUrl) + ".."
+    puts url + ".."
+
+    counter = 0
+    begin
+      newUrl = url + (counter != 0 ? "-#{counter}" : "")
+      counter += 1
+    end while NiceUrl.first(:url => newUrl) != nil
+    self[:niceUrl] = newUrl
+  end
+
+  def sanitize(string)
+    string.downcase.gsub("ö", "oe").gsub("ü", "ue").gsub("ä", "ae").gsub(/\W/,'-').squeeze('-').chomp('-').sub!(/^-*/, '')
+  end
 end
