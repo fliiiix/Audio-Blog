077a7f26f238d38f83c64109b15a57f362a0c4b5
Fliiiix <de-ch@hotmail.de>
2013-11-10 13:20:42 +0100

    fix

--- controller/config.rb
+++ controller/config.rb
@@ -19,9 +19,7 @@ configure :production do
   set :views, Proc.new { File.join(root, "../views") }
   set :public_folder, Proc.new { File.join(root, "../public") }
 
-  AppConfig = YAML.load_file(File.expand_path("config.yaml", ENV["$OPENSHIFT_DATA_DIR"]))["production"]
-
-  puts File.expand_path("config.yaml", ENV["$OPENSHIFT_DATA_DIR"])
+  AppConfig = YAML.load_file(File.expand_path("config.yaml", ENV["OPENSHIFT_DATA_DIR"]))["production"]
 
   MongoMapper.connection = Mongo::Connection.new(ENV["OPENSHIFT_MONGODB_DB_HOST"], ENV["OPENSHIFT_MONGODB_DB_PORT"].to_i)
   MongoMapper.database = ENV["OPENSHIFT_APP_NAME"]
