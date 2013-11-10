acd65f882ada9f7906102efeb098082502a74f62
Fliiiix <de-ch@hotmail.de>
2013-11-10 13:03:39 +0100

    debug

--- controller/config.rb
+++ controller/config.rb
@@ -21,6 +21,8 @@ configure :production do
 
   AppConfig = YAML.load_file(File.expand_path("config.yaml", ENV["$OPENSHIFT_DATA_DIR"]))["production"]
 
+  puts File.expand_path("config.yaml", ENV["$OPENSHIFT_DATA_DIR"])
+
   MongoMapper.connection = Mongo::Connection.new(ENV["OPENSHIFT_MONGODB_DB_HOST"], ENV["OPENSHIFT_MONGODB_DB_PORT"].to_i)
   MongoMapper.database = ENV["OPENSHIFT_APP_NAME"]
   MongoMapper.database.authenticate(ENV["OPENSHIFT_MONGODB_DB_USERNAME"], ENV["OPENSHIFT_MONGODB_DB_PASSWORD"])
