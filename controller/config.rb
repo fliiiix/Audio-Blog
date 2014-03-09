6d2f1113c7c72c4d6c761966f3be375813454129
fliiiix <de-ch@hotmail.de>
2014-03-09 14:39:25 +0100

    refactor config.rb

--- controller/config.rb
+++ controller/config.rb
@@ -5,23 +5,30 @@ helpers do
   def protected! ; halt 401 unless admin? ; end
 end
 
+configure do
+  set :views, Proc.new { File.join(root, "../views") }
+  set :public_folder, Proc.new { File.join(root, "../public") }
+
+  Debug = false
+end
+
 configure :development do
   AppConfig = YAML.load_file(File.expand_path("../config.yaml", File.dirname(__FILE__)))["development"]
   MongoMapper.database = 'music'
   set :show_exceptions, true
-  set :views, Proc.new { File.join(root, "../views") }
-  set :public_folder, Proc.new { File.join(root, "../public") }
-  Debug = true
 end
 
 configure :production do
-  set :views, Proc.new { File.join(root, "../views") }
-  set :public_folder, Proc.new { File.join(root, "../public") }
-
   AppConfig = YAML.load_file(File.expand_path("config.yaml", ENV["OPENSHIFT_DATA_DIR"]))["production"]
 
   MongoMapper.connection = Mongo::Connection.new(ENV["OPENSHIFT_MONGODB_DB_HOST"], ENV["OPENSHIFT_MONGODB_DB_PORT"].to_i)
   MongoMapper.database = ENV["OPENSHIFT_APP_NAME"]
   MongoMapper.database.authenticate(ENV["OPENSHIFT_MONGODB_DB_USERNAME"], ENV["OPENSHIFT_MONGODB_DB_PASSWORD"])
-  Debug = false
+end
+
+configure :test do
+  AppConfig["BlogTitel"] = ""
+  AppConfig["Description"] = ""
+  AppConfig["User"] = ""
+  AppConfig["Pass"] = ""
 end
