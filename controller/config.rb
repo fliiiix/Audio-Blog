d53ffe7d31002707538d0bbd7390ed3584a01fad
fliiiix <de-ch@hotmail.de>
2015-09-24 20:00:20 +0200

    use mongodb config url syntax

--- controller/config.rb
+++ controller/config.rb
@@ -19,12 +19,10 @@ configure :development do
 end
 
 configure :production do
-  AppConfig = YAML.load_file(File.expand_path("config.yaml", ENV["OPENSHIFT_DATA_DIR"]))["production"]
+  AppConfig = YAML.load_file(File.expand_path("../config.yaml", File.dirname(__FILE__)))["production"]
 
-  MongoMapper.connection = Mongo::Connection.new(ENV["OPENSHIFT_MONGODB_DB_HOST"], ENV["OPENSHIFT_MONGODB_DB_PORT"].to_i)
-  MongoMapper.database = ENV["OPENSHIFT_APP_NAME"]
-  MongoMapper.database.authenticate(ENV["OPENSHIFT_MONGODB_DB_USERNAME"], ENV["OPENSHIFT_MONGODB_DB_PASSWORD"])
-
+  # mongodb://user:pass@host:port/dbname
+  MongoMapper.setup({'production' => {'uri' => ENV['MONGODB_AUDIO_URI']}}, 'production')
   Debug = false
 end
 
